854e2bf87b72c7843347046d0247cdee3b67daf9
jonas-andrade <jonas6116m@outlook.com>
2022-04-02 16:30:05 -0300

    initial setup game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log("[jonas-andeade]@ flap bird game 2022");
+
+
+const sprite = new Image().src = "./sprites.png";
+const canvas = document.querySelector("canvas");
+const contexto = canvas.getContext('2d');
+
