5f3c0b3faa1506aef14176599cc2b759cf694ddd
jonas-andrade <jonas6116m@outlook.com>
2022-04-03 18:14:21 -0300

    add collision on the floor

--- game.js
+++ game.js
@@ -7,6 +7,20 @@ const canvas = document.querySelector("canvas");
 const contexto = canvas.getContext('2d');
 
 
+
+
+function collisionFlappy(fl,ch){
+    const pointOfColision = ch.onCanvasY - fl.onSpriteHeight
+
+    if(fl.onCanvasY == pointOfColision ){
+        return true
+    }else{
+        return false
+    }
+
+
+}
+
 // contents 
 const flapBird = {
     //onSprite
@@ -16,48 +30,59 @@ const flapBird = {
     onSpriteHeight: 24,
     //onCanvas     velocidade ( v = v° + a.t )
     onCanvasX: 10,
-    onCancasY: 50,
+    onCanvasY: 50,
     acceleration: 0.25,
     velocity: 0,
+    salt: 4.6,
+    up(){
+        
+        flapBird.velocity = - flapBird.salt;
+        
+    },
     
     update(){
+        if(collisionFlappy(flapBird,flapFloor)){
+            
+            return;
+        }
         flapBird.velocity += flapBird.acceleration
-        flapBird.onCancasY += flapBird.velocity
+        flapBird.onCanvasY += flapBird.velocity
+     
     },
     drawNow(){
         contexto.drawImage(
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            this.onCanvasX ,this.onCancasY,
+            this.onCanvasX ,this.onCanvasY,
             this.onSpriteWidth, this.onSpriteHeight,
         );
     }
 
 
 }
-const flapFlor = {
+const flapFloor = {
     //onSprite
     onSpriteX: 0,
-    onSpriteY: 611,
+    onSpriteY: 610,
     onSpriteWidth: 224,
     onSpriteHeight: 112,
     //onCanvas
     onCanvasX: 0,
-    onCancasY: canvas.height - 112,
+    onCanvasY: canvas.height - 112,
     drawNow(){
         contexto.drawImage(
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            this.onCanvasX ,this.onCancasY,
+            this.onCanvasX ,this.onCanvasY,
             this.onSpriteWidth, this.onSpriteHeight,
         ),
         contexto.drawImage(
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            (this.onCanvasX + this.onSpriteWidth),this.onCancasY,
+            (this.onCanvasX + this.onSpriteWidth),this.onCanvasY,
             this.onSpriteWidth, this.onSpriteHeight,
         );
     }
@@ -72,7 +97,7 @@ const flapGarden = {
     onSpriteHeight: 200,
     //onCanvas
     onCanvasX: 0,
-    onCancasY: canvas.height - flapFlor.onSpriteHeight,
+    onCanvasY: canvas.height - flapFloor.onSpriteHeight,
     drawNow(){
         contexto.fillStyle = '#70c5ce';
         contexto.fillRect(0,0,canvas.width,canvas.height)
@@ -80,14 +105,14 @@ const flapGarden = {
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            this.onCanvasX ,this.onCancasY - flapFlor.onSpriteHeight,
+            this.onCanvasX ,this.onCanvasY - flapFloor.onSpriteHeight,
             this.onSpriteWidth, this.onSpriteHeight,
         ),
         contexto.drawImage(
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            (this.onCanvasX + this.onSpriteWidth),this.onCancasY - flapFlor.onSpriteHeight,
+            (this.onCanvasX + this.onSpriteWidth),this.onCanvasY - flapFloor.onSpriteHeight,
             this.onSpriteWidth, this.onSpriteHeight,
         );
     }
@@ -102,13 +127,13 @@ const screenStart = {
     onSpriteHeight: 151,
     //onCanvas
     onCanvasX: (canvas.width / 2 ) - 188 / 2,
-    onCancasY: 150,
+    onCanvasY: 150,
     drawNow(){
         contexto.drawImage(
             sprite,
             this.onSpriteX, this.onSpriteY,
             this.onSpriteWidth, this.onSpriteHeight,
-            this.onCanvasX ,this.onCancasY - flapFlor.onSpriteHeight,
+            this.onCanvasX ,this.onCanvasY - flapFloor.onSpriteHeight,
             this.onSpriteWidth, this.onSpriteHeight,
         )
     }
@@ -125,9 +150,12 @@ const screen = {
     INICIO: {
         drawNow(){
             flapGarden.drawNow();
-            flapFlor.drawNow();
+            flapFloor.drawNow();
             flapBird.drawNow();
             screenStart.drawNow();
+        },
+        click(){
+            alterScreen(screen.PLAY);
         },
         update(){
             
@@ -137,9 +165,14 @@ const screen = {
     PLAY: {
         drawNow(){
             flapGarden.drawNow();
-            flapFlor.drawNow();
+            flapFloor.drawNow();
             flapBird.drawNow();
 
+        },
+        click(){
+            
+            flapBird.up();
+            
         },
         update(){
             flapBird.update();
@@ -160,7 +193,9 @@ function animate(){
 
 
 window.addEventListener('click',()=>{
-    alterScreen(screen.PLAY);
+    if(screenActive.click){
+        screenActive.click();
+    }
 });
 
 
