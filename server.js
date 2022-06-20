4200f01d57aefa8d72202911101f9e025a9b1b84
jonas-andrade <jonas6116m@outlook.com>
2022-06-20 13:52:01 -0300

    describe port in terminal

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get("/",(req,res)=>{
     res.render("public/index")
 })
 
-app.listen(7771,()=>console.log("server running..."))
+app.listen(7771,()=>console.log("server running in 7771 port...."))
